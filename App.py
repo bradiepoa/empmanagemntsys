4ae2b0c478007f46d8234b32b2b028482cc804b2
bradiepoa7@gmail.com <bradiepoa7@gmail.com>
2024-01-29 21:10:05 +0300

    added id field

--- App.py
+++ App.py
@@ -2,11 +2,18 @@ from flask import Flask , render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+# db config
 app.secret_key = "Secrete Key"
+
 app.config['SQLALCHEMY_DATABASE_URL'] = 'mysql://root:''@localhost/crude'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
+# creating a database tables
+class Data(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
 
 @app.route('/')
 def index():
