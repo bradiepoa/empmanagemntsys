3ba09a7c661574b1de1d52152e5dd67b1a06f233
bradiepoa7@gmail.com <bradiepoa7@gmail.com>
2024-01-29 21:13:30 +0300

    added phone field

--- App.py
+++ App.py
@@ -16,6 +16,7 @@ class Data(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.string(100))
     email = db.Column(db.string(100))
+    phone = db.Column(db.string(100))
 
 @app.route('/')
 def index():
